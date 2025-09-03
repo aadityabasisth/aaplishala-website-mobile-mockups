934d8ebc6c2cbea474157d8323bcb4ccab2a6573
AadityaBasisth <basisthaaditya@gmail.com>
2025-09-03 16:06:32 +0530

    stable version-1

---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Icons } from "@/components/icons";
+import { Section } from "@/components/section";
+import { easeInOutCubic } from "@/lib/animation";
+import { siteConfig } from "@/lib/config";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+export function Hero() {
+  const { scrollY } = useScroll({
+    offset: ["start start", "end start"],
+  });
+  const y1 = useTransform(scrollY, [0, 300], [100, 0]);
+  const y2 = useTransform(scrollY, [0, 300], [50, 0]);
+  const y3 = useTransform(scrollY, [0, 300], [0, 0]);
+  const y4 = useTransform(scrollY, [0, 300], [50, 0]);
+  const y5 = useTransform(scrollY, [0, 300], [100, 0]);
+
+  return (
+    <Section id="hero" className="min-h-[100vh] w-full overflow-hidden">
+      <main className="mx-auto pt-16 sm:pt-24 md:pt-32 text-center relative px-4">
+        <div className="relative">
+          <motion.div
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.2 }}
+            className="mb-16 relative z-20 flex flex-col items-center justify-center"
+            style={{ transformOrigin: "top" }}
+          >
+            <img src="/rena-logo.png" alt="Rena Logo" className="w-20 h-20 mb-2 mx-auto" />
+            <span className="font-bold text-2xl text-orange-500">AapliShala</span>
+          </motion.div>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.7, ease: easeInOutCubic }}
+            className="max-w-2xl mx-auto text-xl mb-8 font-medium text-balance"
+          >
+            AapliShala simplifies school management with digital attendance, timetables, and instant parent-teacher updates. Ideal for boosting efficiency, transparency, and communication in education.
+          </motion.p>
+          <div className="flex justify-center mb-16">
+            <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer" title="Download from Play Store">
+              <motion.img
+                src="/googleplay-dark.png"
+                alt="Download from Play Store"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1, delay: 1 }}
+                className="w-40 flex-shrink-0 block"
+              />
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-nowrap items-center justify-center gap-4 sm:gap-8 h-auto sm:h-[500px] select-none">
+          <motion.img
+            src="/Device-1.png"
+            alt="iPhone"
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            style={{ y: y1 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="w-40 sm:w-64 h-[333px] sm:h-[500px] flex-shrink-0"
+          />
+          <motion.img
+            src="/Device-2.png"
+            alt="iPhone"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            style={{ y: y2 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="w-40 sm:w-64 h-[333px] sm:h-[500px] flex-shrink-0"
+          />
+          <motion.img
+            src="/Device-3.png"
+            alt="iPhone"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            style={{ y: y3 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="w-40 sm:w-64 h-[333px] sm:h-[500px] flex-shrink-0"
+          />
+          <motion.img
+            src="/Device-4.png"
+            alt="iPhone"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            style={{ y: y4 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="w-40 sm:w-64 h-[333px] sm:h-[500px] flex-shrink-0"
+          />
+          <motion.img
+            src="/Device-5.png"
+            alt="iPhone"
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            style={{ y: y5 }}
+            transition={{ duration: 1, delay: 1 }}
+            className="w-40 sm:w-64 h-[333px] sm:h-[500px] flex-shrink-0"
+          />
+        </div>
+      </main>
+    </Section>
+  );
+}
